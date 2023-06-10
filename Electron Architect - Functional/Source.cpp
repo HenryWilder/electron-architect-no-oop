e9cdf2f2c14b08154fde062f31eb72ce185b3aa7
Henry Wilder <henrythepony@gmail.com>
2023-06-10 17:47:51 -0400

    Add project files.

---
+++ Electron Architect - Functional/Source.cpp
@@ -0,0 +1,91 @@
+#include <raylib.h>
+#include <raymath.h>
+
+#define sign(x) (((x) > (decltype(x))(0)) - ((x) < (decltype(x))(0)))
+
+using Vector1 = float;
+
+#pragma region Vector math
+
+inline Vector2  operator- (Vector2  vec)          { return      Vector2Negate(vec);             }
+inline Vector2  operator+ (Vector2  a, Vector2 b) { return      Vector2Add(a, b);               }
+inline Vector2& operator+=(Vector2& a, Vector2 b) { return (a = Vector2Add(a, b));              }
+inline Vector2  operator+ (Vector2  a, float val) { return      Vector2AddValue(a, val);        }
+inline Vector2& operator+=(Vector2& a, float val) { return (a = Vector2AddValue(a, val));       }
+inline Vector2  operator- (Vector2  a, Vector2 b) { return      Vector2Subtract(a, b);          }
+inline Vector2& operator-=(Vector2& a, Vector2 b) { return (a = Vector2Subtract(a, b));         }
+inline Vector2  operator- (Vector2  a, float val) { return      Vector2SubtractValue(a, val);   }
+inline Vector2& operator-=(Vector2& a, float val) { return (a = Vector2SubtractValue(a, val));  }
+inline Vector2  operator* (Vector2  a, Vector2 b) { return      Vector2Multiply(a, b);          }
+inline Vector2& operator*=(Vector2& a, Vector2 b) { return (a = Vector2Multiply(a, b));         }
+inline Vector2  operator* (Vector2  a, float scl) { return      Vector2Scale(a, scl);           }
+inline Vector2& operator*=(Vector2& a, float scl) { return (a = Vector2Scale(a, scl));          }
+inline Vector2  operator/ (Vector2  a, Vector2 b) { return      Vector2Divide(a, b);            }
+inline Vector2& operator/=(Vector2& a, Vector2 b) { return (a = Vector2Divide(a, b));           }
+inline Vector2  operator/ (Vector2  a, float div) { return      Vector2Scale(a, 1.0f / div);    }
+inline Vector2& operator/=(Vector2& a, float div) { return (a = Vector2Scale(a, 1.0f / div));   }
+
+inline Vector3  operator- (Vector3  vec)          { return      Vector3Negate(vec);             }
+inline Vector3  operator+ (Vector3  a, Vector3 b) { return      Vector3Add(a, b);               }
+inline Vector3& operator+=(Vector3& a, Vector3 b) { return (a = Vector3Add(a, b));              }
+inline Vector3  operator+ (Vector3  a, float val) { return      Vector3AddValue(a, val);        }
+inline Vector3& operator+=(Vector3& a, float val) { return (a = Vector3AddValue(a, val));       }
+inline Vector3  operator- (Vector3  a, Vector3 b) { return      Vector3Subtract(a, b);          }
+inline Vector3& operator-=(Vector3& a, Vector3 b) { return (a = Vector3Subtract(a, b));         }
+inline Vector3  operator- (Vector3  a, float val) { return      Vector3SubtractValue(a, val);   }
+inline Vector3& operator-=(Vector3& a, float val) { return (a = Vector3SubtractValue(a, val));  }
+inline Vector3  operator* (Vector3  a, Vector3 b) { return      Vector3Multiply(a, b);          }
+inline Vector3& operator*=(Vector3& a, Vector3 b) { return (a = Vector3Multiply(a, b));         }
+inline Vector3  operator* (Vector3  a, float scl) { return      Vector3Scale(a, scl);           }
+inline Vector3& operator*=(Vector3& a, float scl) { return (a = Vector3Scale(a, scl));          }
+inline Vector3  operator/ (Vector3  a, Vector3 b) { return      Vector3Divide(a, b);            }
+inline Vector3& operator/=(Vector3& a, Vector3 b) { return (a = Vector3Divide(a, b));           }
+inline Vector3  operator/ (Vector3  a, float div) { return      Vector3Scale(a, 1.0f / div);    }
+inline Vector3& operator/=(Vector3& a, float div) { return (a = Vector3Scale(a, 1.0f / div));   }
+
+#pragma endregion
+
+int main()
+{
+    int windowWidth = 1280;
+    int windowHeight = 720;
+    InitWindow(windowWidth, windowHeight, "My Raylib Program");
+    SetTargetFPS(60);
+
+    /******************************************
+    *   Load textures, shaders, and meshes    *
+    ******************************************/
+
+    // @TODO: Load persistent assets & variables
+
+    while (!WindowShouldClose())
+    {
+        /******************************************
+        *   Simulate frame and update variables   *
+        ******************************************/
+
+        // @TODO: simulate frame
+
+        /******************************************
+        *   Draw the frame                        *
+        ******************************************/
+
+        BeginDrawing(); {
+
+            ClearBackground(BLACK);
+
+            // TODO: Draw frame
+
+        } EndDrawing();
+    }
+
+    /******************************************
+    *   Unload and free memory                *
+    ******************************************/
+
+    // @TODO: Unload variables
+
+    CloseWindow();
+
+	return 0;
+}
