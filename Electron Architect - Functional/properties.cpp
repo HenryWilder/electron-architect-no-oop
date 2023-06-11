73f5a5f36e7dcd9afd49854dd252910350e5340a
Henry Wilder <henrythepony@gmail.com>
2023-06-11 15:46:49 -0400

    Minor change

--- Electron Architect - Functional/properties.cpp
+++ Electron Architect - Functional/properties.cpp
@@ -3,5 +3,14 @@
 
 namespace properties
 {
-	panel::Panel propertiesPanel = { "Properties", { 1000, &panel::windowBounds.ymin, &panel::windowBounds.xmax, &console::consolePanel.bounds.ymin }, panel::DraggableEdges::EdgeL };
+	panel::Panel propertiesPanel = {
+		"Properties",
+		{
+			1000,
+			&panel::windowBounds.ymin,
+			&panel::windowBounds.xmax,
+			&console::consolePanel.bounds.ymin
+		},
+		(panel::DraggableEdges)((int)panel::DraggableEdges::EdgeL | (int)panel::DraggableEdges::EdgeB)
+	};
 }
